1dbceffede09404f33ea8fce78eb4d2650f2b944
ccthecode <ccthecode@gmail.com>
2023-10-21 16:01:54 +0100

    Fix List Page Error

--- src/routing/ContactPage.tsx
+++ src/routing/ContactPage.tsx
@@ -4,12 +4,15 @@ const ContactPage = () => {
   const navigate = useNavigate()
 
   return (
-    <form onSubmit={(event)=> {
-      event.preventDefault();
-      navigate("/")
-    }}>
-      <button>Submit</button>
-    </form>
+    <div className="p-6">
+      <h3>Contact Page</h3>
+      <form onSubmit={(event)=> {
+        event.preventDefault();
+        navigate("/")
+      }}>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
   )
 }
 
